51d7a19555281ab965df4f5a17f4ea169b661333
Atsushi Morimoto (74th) <site@74th.tech>
2020-10-04 15:47:04 +0900

    wip

--- index.js
+++ index.js
@@ -21,7 +21,8 @@ var __importStar = (this && this.__importStar) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const core = __importStar(require("@actions/core"));
 const github = __importStar(require("@actions/github"));
-const action_1 = require("@octokit/action");
+const fs = __importStar(require("fs/promises"));
+// import { Octokit } from "@octokit/action";
 function isUserPermittedByUserName(actor) {
     const input = core.getInput("users");
     if (!input) {
@@ -30,29 +31,50 @@ function isUserPermittedByUserName(actor) {
     const users = input.split(",");
     return users.findIndex(user => user === actor) > -1;
 }
-async function isUserPermittedByTeam(actor) {
-    const input = core.getInput("teams");
+// listMembersInOrg is not accessible by integration
+// async function isUserPermittedByTeam(actor: string): Promise<boolean> {
+//     const input = core.getInput("teams");
+//     if (!input) {
+//         return false;
+//     }
+//     const teams = input.split(",");
+//     const octokit = new Octokit();
+//     for (const team of teams) {
+//         try {
+//             for await (const res of octokit.paginate.iterator(
+//                 octokit.teams.listMembersInOrg,
+//                 {
+//                     org: github.context.repo.owner,
+//                     team_slug: team,
+//                 }
+//             )) {
+//                 if (res.data.findIndex((user) => { return user.login == actor; }) > -1) {
+//                     return true;
+//                 }
+//             }
+//         } catch (e) {
+//             core.error(`error occurred when fetch team ${github.context.repo.owner}/${team}`);
+//             core.error(e);
+//             return false;
+//         }
+//     }
+//     return false;
+// }
+async function isUserPermittedByListfile(actor) {
+    const input = core.getInput("listfile");
     if (!input) {
         return false;
     }
-    const teams = input.split(",");
-    const octokit = new action_1.Octokit();
-    for (const team of teams) {
-        try {
-            for await (const res of octokit.paginate.iterator(octokit.teams.listMembersInOrg, {
-                org: github.context.repo.owner,
-                team_slug: team,
-            })) {
-                if (res.data.findIndex((user) => { return user.login == actor; }) > -1) {
-                    return true;
-                }
-            }
+    try {
+        const file = await fs.readFile(input);
+        const members = file.toString().trim().split("\n");
+        if (members.findIndex((member) => { return member == actor; }) > -1) {
+            return true;
         }
-        catch (e) {
-            core.error(`error occurred when fetch team ${github.context.repo.owner}/${team}`);
-            core.error(e);
-            return false;
-        }
+    }
+    catch (e) {
+        core.error(`error occurred when load listfile`);
+        core.error(e);
     }
     return false;
 }
@@ -62,13 +84,16 @@ async function main() {
         core.info("permitted by users");
         return;
     }
-    core.info("not permitted by users");
-    const result = await isUserPermittedByTeam(actor);
-    if (result) {
-        core.info("permitted by teams");
+    // const result = await isUserPermittedByTeam(actor);
+    // if (result) {
+    //     core.info("permitted by teams");
+    //     return
+    // }
+    const permitted = await isUserPermittedByListfile(actor);
+    if (permitted) {
+        core.info("permitted by listfile");
         return;
     }
-    core.info("not permitted by teams");
     core.setFailed(`${actor} is not permitted this workflow`);
 }
 main();
--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import * as core from '@actions/core';
 import * as github from '@actions/github';
-import { Octokit } from "@octokit/action";
+import * as fs from 'fs/promises';
+// import { Octokit } from "@octokit/action";
 
 function isUserPermittedByUserName(actor: string): boolean {
     const input = core.getInput("users");
@@ -13,31 +14,51 @@ function isUserPermittedByUserName(actor: string): boolean {
 }
 
 
-async function isUserPermittedByTeam(actor: string): Promise<boolean> {
-    const input = core.getInput("teams");
+// listMembersInOrg is not accessible by integration
+// async function isUserPermittedByTeam(actor: string): Promise<boolean> {
+//     const input = core.getInput("teams");
+//     if (!input) {
+//         return false;
+//     }
+//     const teams = input.split(",");
+//     const octokit = new Octokit();
+//     for (const team of teams) {
+//         try {
+//             for await (const res of octokit.paginate.iterator(
+//                 octokit.teams.listMembersInOrg,
+//                 {
+//                     org: github.context.repo.owner,
+//                     team_slug: team,
+//                 }
+//             )) {
+//                 if (res.data.findIndex((user) => { return user.login == actor; }) > -1) {
+//                     return true;
+//                 }
+//             }
+//         } catch (e) {
+//             core.error(`error occurred when fetch team ${github.context.repo.owner}/${team}`);
+//             core.error(e);
+//             return false;
+//         }
+//     }
+//     return false;
+// }
+
+async function isUserPermittedByListfile(actor: string) {
+    const input = core.getInput("listfile");
     if (!input) {
         return false;
     }
-    const teams = input.split(",");
-    const octokit = new Octokit();
-    for (const team of teams) {
-        try {
-            for await (const res of octokit.paginate.iterator(
-                octokit.teams.listMembersInOrg,
-                {
-                    org: github.context.repo.owner,
-                    team_slug: team,
-                }
-            )) {
-                if (res.data.findIndex((user) => { return user.login == actor; }) > -1) {
-                    return true;
-                }
-            }
-        } catch (e) {
-            core.error(`error occurred when fetch team ${github.context.repo.owner}/${team}`);
-            core.error(e);
-            return false;
+
+    try {
+        const file = await fs.readFile(input);
+        const members = file.toString().trim().split("\n");
+        if (members.findIndex((member) => { return member == actor; }) > -1) {
+            return true;
         }
+    } catch (e) {
+        core.error(`error occurred when load listfile`);
+        core.error(e);
     }
     return false;
 }
@@ -48,13 +69,19 @@ async function main() {
         core.info("permitted by users");
         return;
     }
-    core.info("not permitted by users");
-    const result = await isUserPermittedByTeam(actor);
-    if (result) {
-        core.info("permitted by teams");
-        return
+
+    // const result = await isUserPermittedByTeam(actor);
+    // if (result) {
+    //     core.info("permitted by teams");
+    //     return
+    // }
+
+    const permitted = await isUserPermittedByListfile(actor);
+    if (permitted) {
+        core.info("permitted by listfile");
+        return;
     }
-    core.info("not permitted by teams");
+
     core.setFailed(`${actor} is not permitted this workflow`)
 }
 
