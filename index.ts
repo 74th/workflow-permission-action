745f567a5e33435d2b8ac32d5f268e850d9d191b
Atsushi Morimoto (74th) <site@74th.tech>
2020-10-04 13:46:04 +0900

    wip

--- index.ts
+++ index.ts
@@ -1,12 +1,30 @@
 import * as core from '@actions/core';
 import * as github from '@actions/github';
+import { Octokit } from "@octokit/action";
 
-core.setCommandEcho(true)
-const users = core.getInput("users").split(",");
-const actor = github.context.actor;
-core.warning("@@1" + core.getInput("users"))
-const available = users.find(user => user === actor);
-core.warning("@@2" + available)
-if (available == undefined) {
-    core.setFailed(`${actor} is not permitted this workflow`)
+async function isUserPermittedByUserName(actor: string): boolean {
+    const users = core.getInput("users").split(",");
+    const available = users.find(user => user === actor);
+    return available !== undefined;
 }
+
+async function isUserPermittedByTeam(actor: string): boolean {
+    const groupNames = core.getInput("groups").split(",");
+    const octokit = new Octokit();
+    await octokit.orgs.get({ org: github.context.repo.owner })
+    for (const groupName in groupNames) {
+
+        if (groupName.length == 0) continue;
+    }
+}
+
+async function main() {
+    const actor = github.context.actor;
+    const permittedByUser = isUserPermittedByUserName(actor);
+    if (permittedByUser) {
+        return;
+    }
+    core.setFailed(`${actor} is not permitted this workflow`)
+    return;
+}
+main();
