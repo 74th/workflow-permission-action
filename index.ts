8a45a241fca2b8ace51e1f8c53deae647f73d3ac
Atsushi Morimoto (74th) <site@74th.tech>
2020-10-04 12:53:04 +0900

    add debug log

--- index.ts
+++ index.ts
@@ -3,7 +3,9 @@ import * as github from '@actions/github';
 
 const users = core.getInput("users").split(",");
 const actor = github.context.actor;
+core.info("@@1" + core.getInput("users"))
 const available = users.find(user => user === actor);
+core.info("@@2" + available)
 if (available == undefined) {
     core.setFailed(`${actor} is not permitted this workflow`)
 }
